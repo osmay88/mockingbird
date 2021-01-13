b8267addfb50648aa8f8931db4ba6fb322c1cc89
Osmay Cruz <osmay.cruz@gmail.com>
2021-01-13 12:32:01 -0500

    Refactor dynamo repository

--- mockingbird/repository/dynamo_repository.py
+++ mockingbird/repository/dynamo_repository.py
@@ -17,23 +17,23 @@ class DynamoRepository:
         response = table.put_item(Item=item)
         return response
 
-    def get_stubs(self, id=None, namespace=None):
-        log.info("Getting stub with id %s" % id)
+    def get_stubs(self, stub_id=None, namespace=None):
+        log.info("Getting stub with id %s" % stub_id)
         table = self._client.Table(STUBS_TABLE)
         if not id and not namespace:
             return table.scan().get("Items", [])
-        response = table.query(KeyConditionExpression=Key('id').eq(id))
+        response = table.query(KeyConditionExpression=Key('id').eq(stub_id))
         return response.get("Items")[0]
 
     def store_stub(self, item: dict):
         return self._put_item_in_table(STUBS_TABLE, item)
 
-    def clean_stub(self, filter=None):
+    def clean_stub(self, filter_query=None):
         pass
 
-    def get_url_hash(self, hash: str):
+    def get_url_hash(self, hash_url: str):
         table = self._client.Table(URL_HASH_TABLE)
-        result = table.query(KeyConditionExpression=Key('url_hash').eq(hash))
+        result = table.query(KeyConditionExpression=Key('url_hash').eq(hash_url))
         return result.get("Items", [])
 
     def store_url_hash(self, item: dict):
