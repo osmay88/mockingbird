e0bac2b85a4fe2a43be24be4b466eea68ed2441b
Osmay Cruz <osmay.cruz@gmail.com>
2021-01-26 04:47:33 -0500

    Added runner in flask
    This runner in flask help with locally
    debugging the application

--- web_runner/main_app.py
+++ web_runner/main_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from mockingbird.services.request_manager import handle_request
 from mockingbird.services.stub_manager import get_stub
@@ -7,18 +7,22 @@ from mockingbird.services.stub_manager import get_stub
 app = Flask(__name__)
 
 
-@app.route("/mock-it/")
-def mock_it(*args, **kwargs):
-    stubs = get_stub()
-    return jsonify(stubs), 200
+@app.route("/mock-it/<namespace>/<path:path>")
+def mock_it(namespace, path, *args, **kwargs):
+    return "You made it", 200
 
 
-@app.route("/create-stub", methods=["POST"])
+@app.route("/__admin/mappings", methods=["POST", "GET"])
 def create_stub(*args, **kwargs):
-    return "Created", 201
+    if request.method == "GET":
+        # TODO: get all the stubs or a specific one
+        return "NO stubs yet", 200
+    elif request.method == "POST":
+        return "Your stub was created", 201
+    else:
+        return "Uh yeah", 204
 
 
-@app.route("/get-stubs/<stub_id>", methods=["GET"])
-def get_stubs(*args, **kwargs):
-    return "ok", 200
+if __name__ == "__main__":
+    app.run()
 
