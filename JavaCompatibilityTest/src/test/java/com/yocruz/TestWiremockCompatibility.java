ac8ddc60a615d85b258a13e7d007a017d71222b0
cruzo <osmay.cruz@gmail.com>
2021-03-18 15:16:54 -0400

    Added Java app to test compatibility with wiremock

---
+++ JavaCompatibilityTest/src/test/java/com/yocruz/TestWiremockCompatibility.java
@@ -0,0 +1,30 @@
+package com.yocruz;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.jupiter.api.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+
+public class TestWiremockCompatibility {
+
+    public TestWiremockCompatibility() {
+        WireMock.configureFor("localhost", 5000);
+    }
+
+
+    @Test
+    public void endpointsCanBeCreated() {
+
+        stubFor(get(urlEqualTo("/my/resource"))
+                .withHeader("Accept", equalTo("text/xml"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "text/xml")
+                        .withBody("<response>Some content</response>")));
+
+    }
+}
