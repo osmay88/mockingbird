1d6f7587237591e3e8a98edefb66dbfabf1edeef
Osmay Cruz <osmay.cruz@gmail.com>
2021-04-20 18:25:04 -0400

    Saved doc about query matching
    may the force be with us

---
+++ mockingbird/services/query_parser.py
@@ -0,0 +1,13 @@
+"""
+create the necesary methods to handle the mockingbird
+query requests here.
+Operators:
+    matches: the values are the same, it can be regular exp.
+    absent: the given value is not there
+
+"""
+
+
+def parse_query(*args, **kwargs):
+    pass
+
